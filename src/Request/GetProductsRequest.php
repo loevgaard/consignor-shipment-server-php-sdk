e80146039d947672a522f707aeaaf92888e41982
Joachim Løvgaard <joachim@loevgaard.dk>
2018-02-27 09:06:10 +0100

    Removed empty response and used the generic response

--- src/Request/GetProductsRequest.php
+++ src/Request/GetProductsRequest.php
@@ -1,17 +1,10 @@
 <?php
 namespace Loevgaard\Consignor\ShipmentServer\Request;
 
-use Loevgaard\Consignor\ShipmentServer\Response\GetProductsResponse;
-
 class GetProductsRequest extends Request
 {
     public function getCommand(): string
     {
         return RequestInterface::COMMAND_GET_PRODUCTS;
-    }
-
-    public function getResponseClass(): string
-    {
-        return GetProductsResponse::class;
     }
 }
--- src/Response/GetProductsResponse.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Loevgaard\Consignor\ShipmentServer\Response;
-
-class GetProductsResponse extends Response
-{
-}
